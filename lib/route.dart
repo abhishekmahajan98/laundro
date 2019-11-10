9f483ceae5640e18e27460b47d2d772e5d4ddfab
SIDDHANT <siddhanthbajoria@gmail.com>
2019-11-10 20:39:45 +0530

    routes modified

---
+++ lib/route.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import './pages/home_page.dart';
+import './pages/login_page.dart';
+import './pages/welcome.dart';
+import './pages/registration_page.dart';
+import './pages/ironing_menu_page.dart';
+import './pages/washing_menu_page.dart';
+import './pages/dry_cleaning_menu_page.dart';
+import './pages/cart_page.dart';
+
+Map<String, WidgetBuilder> routes(BuildContext context) {
+    return <String, WidgetBuilder>{
+    "/": (context) => WelcomeScreen(),
+    "/login": (context) => LoginScreen(),
+    "/register": (context) => RegistrationScreen(),
+    "/home": (context) => HomePage(),
+    "/iron": (context) => IroningMenuPage(),
+    "/wash": (context) => WashingMenuPage(),
+    "/dry-clean": (context) => DryCleaningMenuPage(),
+    "/cart": (context) => CartPage()
+  };
+ }
